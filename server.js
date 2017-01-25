215aff50228b3c22a57452d5da01b1e0495733b2
deepak209g <209deepakmishra@gmail.com>
2017-01-25 19:13:22 +0530

    added config file

--- server.js
+++ server.js
@@ -3,17 +3,28 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var dir = require('node-dir');
+var ip = require('ip');
+var hb = require('handlebars');
 
+var myip = ip.address();
+
+
+var indexhtml = fs.readFileSync(__dirname + '/index.html', "utf8");
+var indexTemplate = hb.compile(indexhtml);
 
 // send the audio player frontend to the client
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  //res.sendFile(__dirname + '/index.html');
+  res.send(indexTemplate({ip: myip}));
 });
 
 
+
+var adminhtml = fs.readFileSync(__dirname + '/admin.html', "utf8");
+var adminTemplate = hb.compile(adminhtml);
 // app adimin panel
 app.get('/admin', function(req, res){
-	res.sendFile(__dirname + '/admin.html');
+	res.send(adminTemplate({ip: myip}));
 });
 
 
@@ -23,23 +34,22 @@ var readStream;
 // a new user is connected
 io.on('connection', function(socket){
 
-	socket.on('more', function(data){
-		console.log("more");
-		var data = readStream.read(524288);
-		var time = Date.now();
-  		io.sockets.emit('moreData', { data: data, time: time });
-  	});
-
-
   	// list files
   	socket.on('listFiles', function(data){
-  		dir.paths("D:\\Music", function(err, paths) {
-		    if (err) throw err;
-		    //console.log('files:\n',paths.files);
-		    //console.log('subdirs:\n', paths.dirs);
-		    var files = paths.files.filter(isMp3File);
-		    socket.emit('fileList', files);
+  		fs.readFile(__dirname + '/config.json', 'utf8', (err, data) => {
+		  if (err) throw err;
+		  var config = JSON.parse(data);
+		  var music_dirs = config.music_directory;
+		  for(var i=0; i<music_dirs.length; i++){
+		  	dir.files(music_dirs[i], function(err, paths) {
+			    if (err) throw err;
+			    var files = paths.filter(isMp3File);
+			    socket.emit('fileList', files);
+			});  	
+		  }
+
 		});
+  		
   	});
 
 
@@ -73,5 +83,5 @@ function isMp3File(file) {
 
 // blah blah
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on '+ myip +':3000');
 });
